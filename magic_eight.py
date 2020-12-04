6f7c583708ea71eabbe2921ffcdfc094e4d25636
GoodGuyAsh <67311318+GoodGuyAsh@users.noreply.github.com>
2020-12-04 12:25:04 +0530

    Minor change to the format of the list.

--- magic_eight.py
+++ magic_eight.py
@@ -8,7 +8,26 @@ Magic 8 ball
 import random
 
 # This list of answers contains 10 affirmative answers, 5 non-commital answer, and 5 negative answers :O
-ANSWERS = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+ANSWERS = ["It is certain.", 
+            "It is decidedly so.", 
+            "Without a doubt.", 
+            "Yes – definitely.", 
+            "You may rely on it.", 
+            "As I see it, yes.", 
+            "Most likely.", 
+            "Outlook good.", 
+            "Yes", 
+            "Signs point to yes.", 
+            "Reply hazy, try again.", 
+            "Ask again later.", 
+            "Better not tell you now.", 
+            "Cannot predict now.", 
+            "Concentrate and ask again.", 
+            "Don't count on it.", 
+            "My reply is no.", 
+            "My sources say no.", 
+            "Outlook not so good.", 
+            "Very doubtful."]
 
 
 random.shuffle(ANSWERS)                                                    # Randomizes the list of the answers for each instance.
